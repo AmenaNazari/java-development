80773ec29d46671bd36fb064cdb35f000d9444da
Amena Nazari <nazara@jevs.org>
2025-04-19 11:30:45 -0400

    completed ShopingList app

---
+++ workbook-1/ShoppingList/src/main/com/pluralsight/ShoppingList.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main (String[]args) {
+        System .out. println ("ShoppingList");
+        System .out.println ("Fruits");
+        System .out.println ("clothes");
+        System .out.println ("Dishes");
+        System .out.println   ("Organizer");
+        System .out.println  ("Books");
+        System .out.println ("Computer");
+        System .out.println  ("Pen");
+        System .out.println  ("Glasses");
+        System .out.println   ("Pencil");
+        System .out.println  ("Ring");
+
+
+
+    }
+
+
+}
